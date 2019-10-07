6c0cd955f2edb137d69be2be47096311e02d8ed3
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2019-10-07 16:26:02 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import messagebox as msg
+
+class Weather():
+    def __init__(self,master):
+        self.master = master
+        self.master.title("Weather App")
+        self.master.geometry("250x200")
+        self.master.resizable(False,False)
+        
+        self.menu = Menu(self.master)
+        
+        self.file_menu = Menu(self.menu,tearoff = 0)
+        self.file_menu.add_command(label = "Add a city" , accelerator = 'Alt+O',self.add_city)
+        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
+        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        
+        self.about_menu = Menu(self.menu,tearoff = 0)
+        self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
+        self.menu.add_cascade(label="About",menu=self.about_menu)
+        
+        self.help_menu = Menu(self.menu,tearoff = 0)
+        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
+        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        
+        self.master.config(menu=self.menu)
+        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
+        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
+        self.master.bind('<Control-i>',lambda event: self.aboutmenu())
+    
+    def add_city(self):
+        pass
+    
+    def exitmenu(self):
+        if msg.askokcancel("Quit?", "Really quit?"):
+            self.master.destroy()
+    
+    def helpmenu(self):
+        pass
+    
+    def aboutmenu(self):
+        pass
+
+        
+
+def main():
+    root=Tk()
+    W = Weather(root)
+    root.mainloop()
+    
+if __name__=='__main__':
+    main()
